153f10fd67608b27e0b5c950beb4857123b3589a
Rafael Leite <rafael.leite@iteris.com.br>
2019-04-26 10:26:37 -0300

    vencedor ok, não clica quando ganha

--- projeto/src/app/board/board.component.ts
+++ projeto/src/app/board/board.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { identifierModuleUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-board',
@@ -7,37 +6,67 @@ import { identifierModuleUrl } from '@angular/compiler';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-  constructor() { this.letra = 'X' || 'O'; }
-  letra: any;
+  constructor() { this.fill = 'X' || 'O'; this.turn = 'X'; }
+  fill: any;
   board: any[];
-  vez: 'X';
+  turn: any;
+  lock: boolean;
+  status: string;
+
 
   ngOnInit() {
     this.start();
-  }
-
-  mostraO() {
-    document.getElementById('mostra').innerHTML = 'O';
-  }
-
-  mostraX() {
-    document.getElementById('mostra').innerHTML = 'X';
   }
 
   play(casa) {
     if (casa.value === 'X' || casa.value === 'O') {
       alert('Quadrado já escolhido, escolha outro');
     } else {
-      casa.value = this.letra;
-      console.log(casa.value);
-      if (this.letra === 'X') {
-        this.letra = 'O';
-        this.mostraO();
-      } else {
-        this.letra = 'X';
-        this.mostraX();
+      casa.value = this.fill;
+      this.verify(this.turn);
+    }
+  }
+
+  get lines(): any[] {
+    return [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  }
+
+  verify(turn) {
+    // vitoria
+    for (const pattern of this.lines) {
+      const foundWinner = this.board[pattern[0]].value === turn
+        && this.board[pattern[1]].value === turn
+        && this.board[pattern[2]].value === turn;
+      if (foundWinner) {
+        for (const index of pattern) {
+          this.board[index].winner = true;
+        }
+        this.lock = true;
+        console.log(this.lock);
+        this.status = 'VENCEDOR!';
+        alert('CABO');
       }
     }
+
+    // muda vez
+    if (this.fill === 'X') {
+      this.fill = 'O';
+      this.turn = this.fill;
+    } else {
+      this.fill = 'X';
+      this.turn = this.fill;
+    }
+    console.log(this.lock);
+    return false;
   }
 
   start() {
@@ -46,6 +75,8 @@ export class BoardComponent implements OnInit {
       { value: '' }, { value: '' }, { value: '' },
       { value: '' }, { value: '' }, { value: '' }
     ];
+    this.status = 'É a vez de: ';
+    this.lock = false;
   }
 
 }
